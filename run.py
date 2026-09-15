7c71188bb39bd0ab064c3a967ea0f3c896aa0dd4
ayomide adebayo <adebayo2001ayomide@gmail.com>
2026-09-15 09:04:03 +0000

    Use PORT env variable for Render deployment

--- run.py
+++ run.py
@@ -1,9 +1,12 @@
+import os
 import uvicorn
 
 if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8000))
+    reload = os.getenv("RENDER", "") == ""
     uvicorn.run(
         "app.main:app",
         host="0.0.0.0",
-        port=8000,
-        reload=True
+        port=port,
+        reload=reload
     )
